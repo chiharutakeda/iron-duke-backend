a9482797009b4ab2f5bf9bdd3e033445108c14a7
Takeda Chiharu <takeda123pc@gmail.com>
2021-04-04 22:23:00 +0900

    add:リファクタ

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,22 @@ import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import Exporess from 'express';
 import { buildSchema } from 'type-graphql';
-import { Resolver, Query, Mutation } from 'type-graphql';
+import {ToDoListResolver} from './Resolver/TodoListResolver'
 
-@Resolver()
-export class ToDoListResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return 'hello World';
-  }
-
-  @Mutation(() => String)
-  async toDoRegiste() {
-    return 'hello World';
-  }
-}
 
 const startApollo = async () => {
+  
   const schema = await buildSchema({
     resolvers: [ToDoListResolver],
   });
+
   const apolloServer = new ApolloServer({
     schema,
     mocks: true,
   });
 
   const app = Exporess();
+  
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
